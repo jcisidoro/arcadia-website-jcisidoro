ddb9757ba8768e3e0b2b18f49be0a27aa42f130e
John Patrick Culi Isidoro <jcisidoro@sscgi.com>
2025-04-03 17:05:10 +0800

    modified footer content

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -20,8 +20,19 @@ export default function Footer() {
       </div>
 
       <div className="flex flex-col lg:flex-row w-full">
+        {/* Left Content */}
         <div className="flex w-full flex-col">
-          {/* Left Content */}
+          <p className="text-white text-sm sm:text-lg xl:text-2xl leading-[2rem] lg:leading-[2.5rem] font-extralight mt-4 text-justify">
+            We understand that every firm has unique needs and requirements.
+            That&apos;s why whether it&apos;s policy consulting, innovation
+            solutions, or resource mobilization, our team will work closely with
+            you to design a tailored approach that addresses your specific needs
+            and challenges.
+          </p>
+        </div>
+
+        {/* Right Content */}
+        <div className="flex flex-col h-auto w-full p-4 items-center justify-center mt-4 lg:mt-0">
           <div className="z-2 text-white w-full h-full flex flex-col">
             {/* Arcadia Logo */}
             <div className="flex w-full items-center">
@@ -34,70 +45,62 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className="z-2 text-white w-full h-full">
-            <p className="text-xs md:text-sm lg:text-lg leading-[2rem] lg:leading-[2.5rem] font-extralight mt-4 text-justify">
-              We understand that every firm has unique needs and requirements.
-              That&apos;s why whether it&apos;s policy consulting, innovation
-              solutions, or resource mobilization, our team will work closely
-              with you to design a tailored approach that addresses your
-              specific needs and challenges.
-            </p>
+          <div className="flex flex-col lg:flex-row w-full h-full z-2">
+            <div className="w-full h-full place-content-end">
+              {/* Contact Information */}
+              {contact.map((person, index) => (
+                <div
+                  key={index}
+                  className="text-white text-sm lg:text-xl mt-4 flex flex-col leading-8"
+                >
+                  <span className="font-semibold">{person.name}</span>
+                  <span>{person.number}</span>
+                  <span>{person.email}</span>
+                </div>
+              ))}
+            </div>
+            <div className="flex flex-col w-full h-full">
+              {/* Object array here */}
+              <div className="flex h-32 w-full items-center justify-center gap-8">
+                {[
+                  {
+                    icon: <FaFacebook />,
+                    iconText: "Like & Follow us on Facebook",
+                  },
+                  {
+                    icon: <FaInstagram />,
+                    iconText: "See our latest updates on Instagram",
+                  },
+                  {
+                    icon: <FaXTwitter />,
+                    iconText: "Join the conversation on Twitter",
+                  },
+                  {
+                    icon: <IoLogoWhatsapp />,
+                    iconText: "Chat with us on WhatsApp",
+                  },
+                ].map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex items-center justify-center group relative"
+                  >
+                    {/* Hover Text (Initially Hidden) */}
+                    <span className="hidden lg:flex flex-col absolute xl:text-nowrap -top-10 bg-black text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                      {item.iconText}
+                    </span>
 
-            {/* Contact Information */}
-            {contact.map((person, index) => (
-              <div
-                key={index}
-                className="text-sm lg:text-xl mt-4 flex flex-col leading-8"
-              >
-                <span className="font-semibold">{person.name}</span>
-                <span>{person.number}</span>
-                <span>{person.email}</span>
+                    {/* Social Media Icon Button */}
+                    <button className="flex text-4xl text-white cursor-pointer hover:scale-110 transition-all duration-300">
+                      {item.icon}
+                    </button>
+                  </div>
+                ))}
               </div>
-            ))}
+              <span className="text-white text-center font-extralight">
+                Be Part of Our Community - Follow Us
+              </span>
+            </div>
           </div>
-        </div>
-
-        {/* Right Content */}
-        <div className="flex flex-col h-auto w-full p-4 items-center justify-center mt-4">
-          {/* Icons here */}
-          <div className="flex h-32 w-full items-center justify-center gap-8">
-            {[
-              {
-                icon: <FaFacebook />,
-                iconText: "Like & Follow us on Facebook",
-              },
-              {
-                icon: <FaInstagram />,
-                iconText: "See our latest updates on Instagram",
-              },
-              {
-                icon: <FaXTwitter />,
-                iconText: "Join the conversation on Twitter",
-              },
-              {
-                icon: <IoLogoWhatsapp />,
-                iconText: "Chat with us on WhatsApp",
-              },
-            ].map((item, index) => (
-              <div
-                key={index}
-                className="flex flex-col items-center group lg:relative"
-              >
-                {/* Hover Text (Initially Hidden) */}
-                <span className="absolute text-nowrap -top-10 bg-black text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                  {item.iconText}
-                </span>
-
-                {/* Social Media Icon Button */}
-                <button className="text-4xl text-white cursor-pointer hover:scale-110 transition-all duration-300">
-                  {item.icon}
-                </button>
-              </div>
-            ))}
-          </div>
-          <span className="text-white text-center font-extralight">
-            Be Part of Our Community - Follow Us
-          </span>
         </div>
       </div>
 
