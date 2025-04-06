69bddfd050292ca2070cb7dc723cff468dafeebb
Jpi <jcisidoro@sscgi.com>
2025-04-06 20:07:59 +0800

    change fetch url

--- client/src/app/pages/UpcomingEvents.tsx
+++ client/src/app/pages/UpcomingEvents.tsx
@@ -24,7 +24,9 @@ export default function UpcomingEvents() {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-        const response = await fetch("http://localhost:3100/api/events");
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/events`
+        );
         const data = await response.json();
         setEvents(data);
       } catch (error) {
