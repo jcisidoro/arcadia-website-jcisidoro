6ab5ec0ca7da5a8c59d7c32ccff2d21280e27d5c
John Patrick Culi Isidoro <jcisidoro@sscgi.com>
2025-04-07 16:59:37 +0800

    removed unused import

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -1,9 +1,4 @@
 import Image from "next/image";
-import { contact } from "@/app/utils/data";
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { IoLogoWhatsapp } from "react-icons/io";
 import Link from "next/link";
 
 export default function Footer() {
