c464615f6c335c4359a15d8a03fe8650b29846a3
John Patrick Culi Isidoro <jcisidoro@sscgi.com>
2025-04-07 16:46:07 +0800

    change footer design

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -4,10 +4,11 @@ import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { IoLogoWhatsapp } from "react-icons/io";
+import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="relative h-full w-full flex flex-col items-center justify-center bg-black/45 p-4 lg:p-12 gap-4">
+    <footer className="relative h-full w-full flex flex-col items-center justify-center bg-black/45 p-4 gap-2">
       {/* Background Image */}
       <div className="absolute inset-0 -z-1">
         <Image
@@ -19,95 +20,69 @@ export default function Footer() {
         />
       </div>
 
-      <div className="flex flex-col lg:flex-row w-full">
-        {/* Left Content */}
-        <div className="flex w-full flex-col">
-          <p className="text-white text-sm sm:text-lg xl:text-2xl leading-[2rem] lg:leading-[2.5rem] font-extralight mt-4 text-justify">
-            We understand that every firm has unique needs and requirements.
-            That&apos;s why whether it&apos;s policy consulting, innovation
-            solutions, or resource mobilization, our team will work closely with
-            you to design a tailored approach that addresses your specific needs
-            and challenges.
-          </p>
-        </div>
-
-        {/* Right Content */}
-        <div className="flex flex-col h-auto w-full p-4 items-center justify-center mt-4 lg:mt-0">
-          <div className="z-2 text-white w-full h-full flex flex-col">
-            {/* Arcadia Logo */}
-            <div className="flex w-full items-center">
-              <div className="w-14 lg:w-24 h-12 lg:h-20 relative">
-                <Image src="/arcadiaLogo1.png" alt="Arcadia Logo" fill />
-              </div>
-              <h1 className="uppercase text-2xl lg:text-5xl font-bold font-cormorant">
-                Get in touch
-              </h1>
-            </div>
+      <div className="flex w-full h-48">
+        <div className="flex flex-col w-full h-full p-8 gap-4">
+          <div className="flex flex-col w-full h-fit">
+            <h1 className="text-white font-semibold uppercase text-2xl leading-4">
+              Sustainability Hub
+            </h1>
+            <span className="text-xs text-white">
+              Smarter Cities, Greener Communities
+            </span>
           </div>
-
-          <div className="flex flex-col lg:flex-row w-full h-full z-2">
-            <div className="w-full h-full place-content-end">
-              {/* Contact Information */}
-              {contact.map((person, index) => (
-                <div
-                  key={index}
-                  className="text-white text-sm lg:text-xl mt-4 flex flex-col leading-8"
-                >
-                  <span className="font-semibold">{person.name}</span>
-                  <span>{person.number}</span>
-                  <span>{person.email}</span>
-                </div>
-              ))}
+          <div className="flex flex-col w-full h-full gap-1 xl:gap-2">
+            <span className="text-white text-xl uppercase font-semibold">
+              Arcadia
+            </span>
+            <p className="text-white text-sm text-justify">
+              Arcadia is a sustainability hub for companies committed to
+              advancing sustainability practices and accelerating the transition
+              to a circular economy.
+            </p>
+          </div>
+        </div>
+        <div className="flex w-full h-full not-first-of-type:justify-between items-center p-8">
+          {[
+            { buttonText: "Events", href: "/pages/events" },
+            { buttonText: "Knowledge Portal", href: "#" },
+            { buttonText: "Initiatives", href: "#" },
+          ].map((item, index) => (
+            <div className="flex flex-col group">
+              <Link
+                key={index}
+                href={item.href}
+                className="text-white cursor-pointer"
+              >
+                <span>{item.buttonText}</span>
+              </Link>
+              <div className="w-full border-1 border-white scale-x-0 origin-center group-hover:scale-x-100 transition-all duration-300 ease-in-out" />
             </div>
-            <div className="flex flex-col w-full h-full">
-              {/* Object array here */}
-              <div className="flex h-32 w-full items-center justify-center gap-8">
-                {[
-                  {
-                    icon: <FaFacebook />,
-                    iconText: "Like & Follow us on Facebook",
-                  },
-                  {
-                    icon: <FaInstagram />,
-                    iconText: "See our latest updates on Instagram",
-                  },
-                  {
-                    icon: <FaXTwitter />,
-                    iconText: "Join the conversation on Twitter",
-                  },
-                  {
-                    icon: <IoLogoWhatsapp />,
-                    iconText: "Chat with us on WhatsApp",
-                  },
-                ].map((item, index) => (
-                  <div
-                    key={index}
-                    className="flex items-center justify-center group relative"
-                  >
-                    {/* Hover Text (Initially Hidden) */}
-                    <span className="hidden lg:flex flex-col absolute xl:text-nowrap -top-10 bg-black text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                      {item.iconText}
-                    </span>
-
-                    {/* Social Media Icon Button */}
-                    <button className="flex text-4xl text-white cursor-pointer hover:scale-110 transition-all duration-300">
-                      {item.icon}
-                    </button>
-                  </div>
-                ))}
-              </div>
-              <span className="text-white text-center font-extralight">
-                Be Part of Our Community - Follow Us
-              </span>
-            </div>
+          ))}
+        </div>
+        <div className="flex flex-col w-full h-full p-8 justify-between">
+          <span className="text-white text-xl font-semibold">Contact Us</span>
+          <div className="flex flex-col text-white">
+            <span>For general inquiries:</span>
+            <span>gsm.arcadia@gmail.com</span>
           </div>
         </div>
       </div>
 
-      <div className="flex text-center w-full justify-center mt-10">
-        <span className="text-white font-extralight">
-          &copy; 2025 Arcadia - Sustainability Hub. All rights reserved.
-        </span>
+      <div className="flex flex-col text-center items-center justify-center w-full">
+        <div className="flex flex-col -space-y-1.5 items-center">
+          <div className="w-10 h-10 relative">
+            <Image
+              src="/arcadiaLogo1.png"
+              alt="Arcadia Logo"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <span className="font-cormorant text-white text-sm uppercase">
+            Arcadia
+          </span>
+        </div>
+        <span className="text-white font-extralight">&copy; 2025 Arcadia</span>
       </div>
     </footer>
   );
