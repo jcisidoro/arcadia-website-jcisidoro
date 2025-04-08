7f10e4f5b19126fe4efc48022ec957718fbdc2b2
John Patrick Culi Isidoro <jcisidoro@sscgi.com>
2025-04-08 09:45:22 +0800

    change url for fetch in back end

--- client/src/app/pages/admin-page/page.tsx
+++ client/src/app/pages/admin-page/page.tsx
@@ -16,7 +16,7 @@ export default function AdminAuthPage() {
 
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/events`,
+        `${process.env.NEXT_PUBLIC_API_URL}/api/admin/login`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
