c8c38f97ab44c019ef7c505af9774db74870d20f
John Patrick Culi Isidoro <jcisidoro@sscgi.com>
2025-04-12 02:53:39 +0800

    modified email style

--- client/src/app/api/email/route.ts
+++ client/src/app/api/email/route.ts
@@ -32,35 +32,38 @@ export async function POST(req: NextRequest) {
       subject:
         "Thank You for Reaching Out to Arcadia - We'll Be in Touch Soon!",
       html: `
-        <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6; padding: 20px;">
+        <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6; padding: 10px;">
           <h2 style="font-size: 24px; color: #326333;">Hello ${firstName} ${lastName},</h2>
-          <p style="font-size: 16px;">Thank you for reaching out to us at Arcadia! We truly appreciate your submission and will review your information as soon as possible.</p>
+          <p style="font-size: 16px; text-align: justify;">Thank you for reaching out to us at Arcadia! We truly appreciate your submission and will review your information as soon as possible.</p>
     
-          <p style="font-size: 16px;">Our team is currently reviewing your message, and we&apos;ll get back to you shortly. Rest assured, we&apos;re here to assist you, and we&apos;ll follow up as soon as we have more information to share.</p>
+          <p style="font-size: 16px; text-align: justify;">Our team is currently reviewing your message, and we&apos;ll get back to you shortly. Rest assured, we&apos;re here to assist you, and we&apos;ll follow up as soon as we have more information to share.</p>
     
           <p style="font-size: 16px;">We look forward to connecting with you soon!</p>
     
           <p style="font-size: 16px;">Warm regards,</p>
           <p style="font-size: 16px; font-weight: bold;">The Arcadia Team</p>
+
+          <hr style="border-top: 2px solid #326333; margin: 30px 0;" />
+
+          <!-- Contact and Notes Section -->
+          <p style="font-size: 20px; font-weight: bold;">Your Submitted Information:</p>
+          <p style="font-size: 16px;"><strong>Name:</strong> ${firstName} ${lastName}</p>
+          <p style="font-size: 16px;"><strong>Contact:</strong> ${contact}</p>
+          <p style="font-size: 16px; text-align: justify;"><strong>Notes:</strong> ${notes}</p>
     
           <hr style="border-top: 2px solid #326333; margin: 30px 0;" />
     
           <p style="font-size: 16px; font-weight: bold;">About Arcadia</p>
-          <p style="font-size: 16px;">Arcadia is a sustainability hub for companies committed to advancing sustainability practices and accelerating the transition to a circular economy. We empower businesses to adopt sustainable practices and contribute to a greener future.</p>
+          <p style="font-size: 16px; text-align: justify;">Arcadia is a sustainability hub for companies committed to advancing sustainability practices and accelerating the transition to a circular economy. We empower businesses to adopt sustainable practices and contribute to a greener future.</p>
     
           <p style="font-size: 16px; font-weight: bold;">Contact Us</p>
           <p style="font-size: 16px;">For general inquiries, please reach out to us at: <a href="mailto:gsm.arcadia@gmail.com" style="color: #326333;">gsm.arcadia@gmail.com</a></p>
-    
-          <!-- Contact and Notes Section -->
-          <p style="font-size: 16px; font-weight: bold;">Your Submitted Information:</p>
-          <p style="font-size: 16px;"><strong>Contact:</strong> ${firstName} ${lastName}</p>
-          <p style="font-size: 16px;"><strong>Contact:</strong> ${contact}</p>
-          <p style="font-size: 16px;"><strong>Notes:</strong> ${notes}</p>
 
-          <p style="text-align: center;">
-            <img src="https://arcadia-website-sustainability-hub.vercel.app/arcadiaLogo1.png" alt="Arcadia Logo" style="width: 150px; height: auto; object-fit: cover; display: block; margin: 20px auto;" />
-          </p>
-    
+          <div style="text-align: center;">
+            <img src="https://arcadia-website-sustainability-hub.vercel.app/arcadiaLogo1.png" alt="Arcadia Logo" style="width: 150px; height: auto; object-fit: cover; display: block; margin: 10px auto;" />
+            <span style="font-family: 'Cormorant', serif; font-size: 32px; font-weight: bold; text-transform: uppercase; ">Arcadia</span>
+          </div>
+
           <p style="font-size: 16px; text-align: center;">Thank you for choosing Arcadia!<br/> This email was sent via Nodemailer & Gmail 🚀</p>
     
           <p style="font-size: 16px; text-align: center; color: #888;">&copy; ${new Date().getFullYear()} Arcadia. All rights reserved.</p>
